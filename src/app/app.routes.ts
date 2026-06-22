39d7f3eb1d323780ed8bf619c70d21ac0457f488
Federico <fedfredbal@gmail.com>
2026-06-22 17:02:12 -0300

    base estetica

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Login } from './components/login/login';
+import { Registro } from './components/registro/registro';
+import { Perfil } from './components/perfil/perfil';
+import { Publicaciones } from './components/publicaciones/publicaciones';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'login', component: Login},
+  {path: 'registro', component: Registro},
+  {path: '', component: Publicaciones},
+
+
+  {path: ':username', component: Perfil}
+];
