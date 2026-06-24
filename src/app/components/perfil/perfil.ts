937b10906f01a144b0ffe18b0d5a9bd10f9662d1
Federico <fedfredbal@gmail.com>
2026-06-23 23:39:40 -0300

    mi perfil

--- src/app/components/perfil/perfil.ts
+++ src/app/components/perfil/perfil.ts
@@ -1,9 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-perfil',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './perfil.html',
-  styleUrl: './perfil.css',
 })
-export class Perfil {}
+export class Perfil {
+
+  private route = inject(ActivatedRoute);
+  private http = inject(HttpClient);
+
+  usuario = signal<any | null>(null);
+  publicaciones = signal<any[]>([]);
+  loading = signal(true);
+  notFound = signal(false);
+
+  constructor() {
+    this.route.params.subscribe(params => {
+      const username = params['username'];
+      this.cargarPerfil(username);
+    });
+  }
+
+  cargarPerfil(username: string) {
+    this.loading.set(true);
+    this.notFound.set(false);
+
+    this.http.get<any>(`http://localhost:3000/usuarios/username/${username}`)
+      .subscribe({
+        next: (usuario) => {
+          if(usuario) {
+            this.usuario.set(usuario);
+            this.cargarPublicaciones(usuario._id);
+          } else {
+            this.loading.set(false);
+            this.notFound.set(true);
+            this.usuario.set(null);
+            this.publicaciones.set([])
+          }
+        },
+        error: (error) => {
+          console.error(error)
+        }
+      });
+  }
+
+  cargarPublicaciones(usuarioId: string) {
+    this.http.get<any[]>(
+      `http://localhost:3000/publicaciones?offset=0&limit=3&sortBy=fecha&order=desc`
+    ).subscribe({
+      next: (pubs) => {
+        const filtradas = pubs.filter(p => p.usuarioId === usuarioId);
+        this.publicaciones.set(filtradas);
+        this.loading.set(false);
+      },
+      error: () => {
+        this.loading.set(false);
+      }
+    });
+  }
+
+  formatearFecha(fecha: string) {
+    const date = new Date(fecha);
+    return `${date.getDate().toString().padStart(2,'0')}-${(date.getMonth()+1)
+      .toString().padStart(2,'0')}-${date.getFullYear()}`;
+  }
+}
